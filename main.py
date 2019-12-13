51a2ec07369f86228c0973504a9365a0a8d8f557
Carlos Rodriguez <crodriguez20@ucol.mx>
2019-12-13 17:04:36 -0600

    count neighbors per cell

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-ROWS = 20
-COLUMNS = 20
+ROWS = 10
+COLUMNS = 10
 
 
 def create_cells(rows, columns):
@@ -14,8 +14,37 @@ def create_cells(rows, columns):
     return cells
 
 
+def get_number_of_neighbors(cell_row, cell_col, cells):
+    neighbors = 0
+    most_left_column: int = 0
+    most_right_column = ROWS - 1
+
+    if cell_col > 0:
+        if cells[cell_row][cell_col - 1]['isAlive']:
+            neighbors += 1
+        most_left_column = cell_col - 1
+
+    if cell_col < ROWS - 1:
+        if cells[cell_row][cell_col + 1]['isAlive']:
+            neighbors += 1
+        most_right_column = cell_col + 1
+
+    if cell_row > 0:
+        for col in range(most_left_column, most_right_column + 1):
+            if cells[cell_row - 1][col]['isAlive']:
+                neighbors += 1
+
+    if cell_row < ROWS - 1:
+        for col in range(most_left_column, most_right_column + 1):
+            if cells[cell_row][col]['isAlive']:
+                neighbors += 1
+
+    return neighbors
+
+
 def main():
     cells = create_cells(ROWS, COLUMNS)
+    get_number_of_neighbors(4, 4, cells)
 
     for row in cells:
         for cell in row:
--- test.py
+++ test.py
@@ -12,3 +12,61 @@ class TestCreateCells(unittest.TestCase):
     def testNot0Rows0Columns(self):
         self.assertListEqual(create_cells(1, 0), [[]], "Should be list of one empty list")
         self.assertListEqual(create_cells(2, 0), [[], []], "Should be list of two empty list")
+
+
+class TestCountNeighbors(unittest.TestCase):
+
+    def testOneCenterIsolated(self):
+        cells = create_cells(10, 10)
+        self.assertEqual(get_number_of_neighbors(4, 4, cells), 0, "Should be 0")
+
+    def testOnCenterSurrounded(self):
+        cells = create_cells(10, 10)
+        for row in range(3, 6):
+            for col in range(3, 6):
+                cells[row][col]['isAlive'] = True
+        self.assertEqual(get_number_of_neighbors(4, 4, cells), 8, "Should be 8")
+
+    def testOnLeftUpperCornerIsolated(self):
+        cells = create_cells(10, 10)
+        self.assertEqual(get_number_of_neighbors(0, 0, cells), 0, "Should be 0")
+
+    def testOnLeftUpperCornerSurrounded(self):
+        cells = create_cells(10, 10)
+        for row in range(0, 2):
+            for col in range(0, 2):
+                cells[row][col]['isAlive'] = True
+        self.assertEqual(get_number_of_neighbors(0, 0, cells), 3, "Should be 3")
+
+    def testOnLeftLowerCornerIsolated(self):
+        cells = create_cells(10, 10)
+        self.assertEqual(get_number_of_neighbors(9, 0, cells), 0, "Should be 0")
+
+    def testOnLeftLowerCornerSurrounded(self):
+        cells = create_cells(10, 10)
+        for row in range(8, 10):
+            for col in range(0, 2):
+                cells[row][col]['isAlive'] = True
+        self.assertEqual(get_number_of_neighbors(9, 0, cells), 3, "Should be 3")
+
+    def testOnRightUpperCornerIsolated(self):
+        cells = create_cells(10, 10)
+        self.assertEqual(get_number_of_neighbors(0, 9, cells), 0, "Should be 0")
+
+    def testOnRightUpperCornerSurrounded(self):
+        cells = create_cells(10, 10)
+        for row in range(0, 2):
+            for col in range(8, 10):
+                cells[row][col]['isAlive'] = True
+        self.assertEqual(get_number_of_neighbors(0, 9, cells), 3, "Should be 3")
+
+    def testOnRightLowerCornerIsolated(self):
+        cells = create_cells(10, 10)
+        self.assertEqual(get_number_of_neighbors(9, 9, cells), 0, "Should be 0")
+
+    def testOnRightLowerCornerSurrounded(self):
+        cells = create_cells(10, 10)
+        for row in range(8, 10):
+            for col in range(8, 10):
+                cells[row][col]['isAlive'] = True
+        self.assertEqual(get_number_of_neighbors(9, 9, cells), 3, "Should be 3")
