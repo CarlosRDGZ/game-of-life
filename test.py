09f85ef236086da6c733747e180402f4925b51a0
Carlos Rodriguez <crodriguez20@ucol.mx>
2019-12-13 21:23:53 -0600

    correction of tests' boundaries

--- test.py
+++ test.py
@@ -1,120 +1,120 @@
 import unittest
-from main import *
+import main as game
 
 
 class TestCreateCells(unittest.TestCase):
 
     def testNoRowsNColumns(self):
-        self.assertListEqual([], create_cells(0, 0), "Should be empty list")
-        self.assertListEqual([], create_cells(0, 1), "Should be empty list")
-        self.assertListEqual([], create_cells(0, 2), "Should be empty list")
+        self.assertListEqual([], game.create_cells(0, 0), "Should be empty list")
+        self.assertListEqual([], game.create_cells(0, 1), "Should be empty list")
+        self.assertListEqual([], game.create_cells(0, 2), "Should be empty list")
 
     def testNot0Rows0Columns(self):
-        self.assertListEqual([[]], create_cells(1, 0), "Should be list of one empty list")
-        self.assertListEqual([[], []], create_cells(2, 0), "Should be list of two empty list")
+        self.assertListEqual([[]], game.create_cells(1, 0), "Should be list of one empty list")
+        self.assertListEqual([[], []], game.create_cells(2, 0), "Should be list of two empty list")
 
     def testNot0RowsNot0Columns(self):
-        self.assertListEqual([[False]], create_cells(1, 1), "Should be list of a list with one element")
-        self.assertListEqual([[False, False], [False, False]],create_cells(2, 2), 
+        self.assertListEqual([[False]], game.create_cells(1, 1), "Should be list of a list with one element")
+        self.assertListEqual([[False, False], [False, False]], game.create_cells(2, 2),
                              "Should be list of two lists of two elements each")
 
 
 class TestCountNeighbors(unittest.TestCase):
 
     def testOneCenterIsolated(self):
-        cells = create_cells(10, 10)
-        self.assertEqual(0, get_number_of_neighbors(4, 4, cells), "Should be 0")
+        cells = game.create_cells(game.ROWS, game.COLUMNS)
+        self.assertEqual(0, game.get_number_of_neighbors(4, 4, cells), "Should be 0")
 
     def testOnCenterSurrounded(self):
-        cells = create_cells(10, 10)
+        cells = game.create_cells(game.ROWS, game.COLUMNS)
         for row in range(3, 6):
             for col in range(3, 6):
                 cells[row][col] = True
-        self.assertEqual(8, get_number_of_neighbors(4, 4, cells), "Should be 8")
+        self.assertEqual(8, game.get_number_of_neighbors(4, 4, cells), "Should be 8")
 
     def testOnLeftUpperCornerIsolated(self):
-        cells = create_cells(10, 10)
-        self.assertEqual(0, get_number_of_neighbors(0, 0, cells), "Should be 0")
+        cells = game.create_cells(game.ROWS, game.COLUMNS)
+        self.assertEqual(0, game.get_number_of_neighbors(0, 0, cells), "Should be 0")
 
     def testOnLeftUpperCornerSurrounded(self):
-        cells = create_cells(10, 10)
+        cells = game.create_cells(game.ROWS, game.COLUMNS)
         for row in range(0, 2):
             for col in range(0, 2):
                 cells[row][col] = True
-        self.assertEqual(3, get_number_of_neighbors(0, 0, cells), "Should be 3")
+        self.assertEqual(3, game.get_number_of_neighbors(0, 0, cells), "Should be 3")
 
     def testOnLeftLowerCornerIsolated(self):
-        cells = create_cells(10, 10)
-        self.assertEqual(0, get_number_of_neighbors(9, 0, cells), "Should be 0")
+        cells = game.create_cells(game.ROWS, game.COLUMNS)
+        self.assertEqual(0, game.get_number_of_neighbors(game.ROWS - 1, 0, cells), "Should be 0")
 
     def testOnLeftLowerCornerSurrounded(self):
-        cells = create_cells(10, 10)
-        for row in range(8, 10):
+        cells = game.create_cells(game.ROWS, game.COLUMNS)
+        for row in range(game.ROWS - 2, game.ROWS):
             for col in range(0, 2):
                 cells[row][col] = True
-        self.assertEqual(3, get_number_of_neighbors(9, 0, cells), "Should be 3")
+        self.assertEqual(3, game.get_number_of_neighbors(game.ROWS - 1, 0, cells), "Should be 3")
 
     def testOnRightUpperCornerIsolated(self):
-        cells = create_cells(10, 10)
-        self.assertEqual(0, get_number_of_neighbors(0, 9, cells), "Should be 0")
+        cells = game.create_cells(game.ROWS, game.COLUMNS)
+        self.assertEqual(0, game.get_number_of_neighbors(0, game.COLUMNS - 1, cells), "Should be 0")
 
     def testOnRightUpperCornerSurrounded(self):
-        cells = create_cells(10, 10)
+        cells = game.create_cells(game.ROWS, game.COLUMNS)
         for row in range(0, 2):
-            for col in range(8, 10):
+            for col in range(game.ROWS - 2, game.ROWS):
                 cells[row][col] = True
-        self.assertEqual(3, get_number_of_neighbors(0, 9, cells), "Should be 3")
+        self.assertEqual(3, game.get_number_of_neighbors(0, game.COLUMNS - 1, cells), "Should be 3")
 
     def testOnRightLowerCornerIsolated(self):
-        cells = create_cells(10, 10)
-        self.assertEqual(0, get_number_of_neighbors(9, 9, cells), "Should be 0")
+        cells = game.create_cells(game.ROWS, game.COLUMNS)
+        self.assertEqual(0, game.get_number_of_neighbors(game.ROWS - 1, game.COLUMNS - 1, cells), "Should be 0")
 
     def testOnRightLowerCornerSurrounded(self):
-        cells = create_cells(10, 10)
-        for row in range(8, 10):
-            for col in range(8, 10):
+        cells = game.create_cells(game.ROWS, game.COLUMNS)
+        for row in range(game.ROWS - 2, game.ROWS):
+            for col in range(game.ROWS - 2, game.ROWS):
                 cells[row][col] = True
-        self.assertEqual(3, get_number_of_neighbors(9, 9, cells), "Should be 3")
+        self.assertEqual(3, game.get_number_of_neighbors(game.ROWS - 1, game.COLUMNS - 1, cells), "Should be 3")
 
 
 class TestCellState(unittest.TestCase):
 
     def testCellIsolated(self):
-        cells = create_cells(10, 10)
-        self.assertEqual(False, should_live(0, 0, cells), "Should stay dead")
+        cells = game.create_cells(game.ROWS, game.COLUMNS)
+        self.assertEqual(False, game.should_live(0, 0, cells), "Should stay dead")
         cells[0][0] = True
-        self.assertEqual(False, should_live(0, 0, cells), "Should die")
+        self.assertEqual(False, game.should_live(0, 0, cells), "Should die")
 
     def testNextToOne(self):
-        cells = create_cells(10, 10)
+        cells = game.create_cells(game.ROWS, game.COLUMNS)
         cells[0][1] = True
-        self.assertEqual(False, should_live(0, 0, cells), "Should stay dead")
+        self.assertEqual(False, game.should_live(0, 0, cells), "Should stay dead")
         cells[0][0] = True
-        self.assertEqual(False, should_live(0, 0, cells), "Should die")
+        self.assertEqual(False, game.should_live(0, 0, cells), "Should die")
 
     def testNextToTwo(self):
-        cells = create_cells(10, 10)
+        cells = game.create_cells(game.ROWS, game.COLUMNS)
         cells[0][1] = True
         cells[1][0] = True
-        self.assertEqual(False, should_live(0, 0, cells), "Should stay dead")
+        self.assertEqual(False, game.should_live(0, 0, cells), "Should stay dead")
         cells[0][0] = True
-        self.assertEqual(True, should_live(0, 0, cells), "Should stay alive")
+        self.assertEqual(True, game.should_live(0, 0, cells), "Should stay alive")
 
     def testNextToThree(self):
-        cells = create_cells(10, 10)
+        cells = game.create_cells(game.ROWS, game.COLUMNS)
         cells[0][1] = True
         cells[1][0] = True
         cells[1][1] = True
-        self.assertEqual(True, should_live(0, 0, cells), "Should born")
+        self.assertEqual(True, game.should_live(0, 0, cells), "Should born")
         cells[0][0] = True
-        self.assertEqual(True, should_live(0, 0, cells), "Should stay alive")
+        self.assertEqual(True, game.should_live(0, 0, cells), "Should stay alive")
 
     def testNextToMoreThanThree(self):
-        cells = create_cells(10, 10)
+        cells = game.create_cells(game.ROWS, game.COLUMNS)
         cells[0][0] = True
         cells[0][1] = True
         cells[0][2] = True
         cells[1][0] = True
-        self.assertEqual(False, should_live(1, 1, cells), "Should stay dead")
+        self.assertEqual(False, game.should_live(1, 1, cells), "Should stay dead")
         cells[0][0] = True
-        self.assertEqual(False, should_live(1, 1, cells), "Should die")
+        self.assertEqual(False, game.should_live(1, 1, cells), "Should die")
