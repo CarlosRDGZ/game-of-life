207fe711e0ac58fc0787c57f2504252b7f28f3ce
Carlos Rodriguez <crodriguez20@ucol.mx>
2019-12-13 18:28:09 -0600

    check future state of the cell

--- main.py
+++ main.py
@@ -7,10 +7,7 @@ def create_cells(rows, columns):
     for row in range(rows):
         cells.append([])
         for col in range(columns):
-            cells[row].append({
-                'isAlive': False,
-                'neighbors': 0
-            })
+            cells[row].append(False)
     return cells
 
 
@@ -20,26 +17,37 @@ def get_number_of_neighbors(cell_row, cell_col, cells):
     most_right_column = ROWS - 1
 
     if cell_col > 0:
-        if cells[cell_row][cell_col - 1]['isAlive']:
+        if cells[cell_row][cell_col - 1]:
             neighbors += 1
         most_left_column = cell_col - 1
 
     if cell_col < ROWS - 1:
-        if cells[cell_row][cell_col + 1]['isAlive']:
+        if cells[cell_row][cell_col + 1]:
             neighbors += 1
         most_right_column = cell_col + 1
 
     if cell_row > 0:
         for col in range(most_left_column, most_right_column + 1):
-            if cells[cell_row - 1][col]['isAlive']:
+            if cells[cell_row - 1][col]:
                 neighbors += 1
 
     if cell_row < ROWS - 1:
         for col in range(most_left_column, most_right_column + 1):
-            if cells[cell_row][col]['isAlive']:
+            if cells[cell_row + 1][col]:
                 neighbors += 1
 
     return neighbors
+
+
+def should_live(cell_row, cell_col, cells):
+    is_alive = cells[cell_row][cell_col]
+    neighbors = get_number_of_neighbors(cell_row, cell_col, cells)
+
+    if is_alive and 2 <= neighbors <= 3:
+        return True
+    elif not is_alive and neighbors == 3:
+        return True
+    return False
 
 
 def main():
--- test.py
+++ test.py
@@ -13,6 +13,11 @@ class TestCreateCells(unittest.TestCase):
         self.assertListEqual(create_cells(1, 0), [[]], "Should be list of one empty list")
         self.assertListEqual(create_cells(2, 0), [[], []], "Should be list of two empty list")
 
+    def testNot0RowsNot0Columns(self):
+        self.assertListEqual(create_cells(1, 1), [[False]], "Should be list of a list with one element")
+        self.assertListEqual(create_cells(2, 2), [[False, False], [False, False]],
+                             "Should be list of two lists of two elements each")
+
 
 class TestCountNeighbors(unittest.TestCase):
 
@@ -24,7 +29,7 @@ class TestCountNeighbors(unittest.TestCase):
         cells = create_cells(10, 10)
         for row in range(3, 6):
             for col in range(3, 6):
-                cells[row][col]['isAlive'] = True
+                cells[row][col] = True
         self.assertEqual(get_number_of_neighbors(4, 4, cells), 8, "Should be 8")
 
     def testOnLeftUpperCornerIsolated(self):
@@ -35,7 +40,7 @@ class TestCountNeighbors(unittest.TestCase):
         cells = create_cells(10, 10)
         for row in range(0, 2):
             for col in range(0, 2):
-                cells[row][col]['isAlive'] = True
+                cells[row][col] = True
         self.assertEqual(get_number_of_neighbors(0, 0, cells), 3, "Should be 3")
 
     def testOnLeftLowerCornerIsolated(self):
@@ -46,7 +51,7 @@ class TestCountNeighbors(unittest.TestCase):
         cells = create_cells(10, 10)
         for row in range(8, 10):
             for col in range(0, 2):
-                cells[row][col]['isAlive'] = True
+                cells[row][col] = True
         self.assertEqual(get_number_of_neighbors(9, 0, cells), 3, "Should be 3")
 
     def testOnRightUpperCornerIsolated(self):
@@ -57,7 +62,7 @@ class TestCountNeighbors(unittest.TestCase):
         cells = create_cells(10, 10)
         for row in range(0, 2):
             for col in range(8, 10):
-                cells[row][col]['isAlive'] = True
+                cells[row][col] = True
         self.assertEqual(get_number_of_neighbors(0, 9, cells), 3, "Should be 3")
 
     def testOnRightLowerCornerIsolated(self):
@@ -68,5 +73,48 @@ class TestCountNeighbors(unittest.TestCase):
         cells = create_cells(10, 10)
         for row in range(8, 10):
             for col in range(8, 10):
-                cells[row][col]['isAlive'] = True
+                cells[row][col] = True
         self.assertEqual(get_number_of_neighbors(9, 9, cells), 3, "Should be 3")
+
+
+class TestCellState(unittest.TestCase):
+
+    def testCellIsolated(self):
+        cells = create_cells(10, 10)
+        self.assertEqual(should_live(0, 0, cells), False, "Should stay dead")
+        cells[0][0] = True
+        self.assertEqual(should_live(0, 0, cells), False, "Should die")
+
+    def testNextToOne(self):
+        cells = create_cells(10, 10)
+        cells[0][1] = True
+        self.assertEqual(should_live(0, 0, cells), False, "Should stay dead")
+        cells[0][0] = True
+        self.assertEqual(should_live(0, 0, cells), False, "Should die")
+
+    def testNextToTwo(self):
+        cells = create_cells(10, 10)
+        cells[0][1] = True
+        cells[1][0] = True
+        self.assertEqual(should_live(0, 0, cells), False, "Should stay dead")
+        cells[0][0] = True
+        self.assertEqual(should_live(0, 0, cells), True, "Should stay alive")
+
+    def testNextToThree(self):
+        cells = create_cells(10, 10)
+        cells[0][1] = True
+        cells[1][0] = True
+        cells[1][1] = True
+        self.assertEqual(should_live(0, 0, cells), True, "Should born")
+        cells[0][0] = True
+        self.assertEqual(should_live(0, 0, cells), True, "Should stay alive")
+
+    def testNextToMoreThanThree(self):
+        cells = create_cells(10, 10)
+        cells[0][0] = True
+        cells[0][1] = True
+        cells[0][2] = True
+        cells[1][0] = True
+        self.assertEqual(should_live(1, 1, cells), False, "Should stay dead")
+        cells[0][0] = True
+        self.assertEqual(should_live(1, 1, cells), False, "Should die")
